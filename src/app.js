9471f0defc7e24a5da7c1efce2d20b77fed72552
Laura Vavz <vavzviewz@gmail.com>
2021-04-26 15:13:36 +0200

    Corrected API URL error

--- src/app.js
+++ src/app.js
@@ -5,5 +5,5 @@ function displayTemperature(response) {
 
 
 let apiKey = "c9b707c9a800bc82a565bfc394bcf972";
-let apiUrl = "api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=c9b707c9a800bc82a565bfc394bcf972&units=metric";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=c9b707c9a800bc82a565bfc394bcf972&units=metric";
 axios.get(apiUrl).then(displayTemperature);
