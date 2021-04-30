39de6d98a292487b4c9dfa91c697b1db69497695
Laura Vavz <vavzviewz@gmail.com>
2021-04-30 12:11:50 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -37,15 +37,19 @@ function displayTemperature(response) {
   getForecast(response.data.coord);
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
+function search(city) {
+let apiKey = "c9b707c9a800bc82a565bfc394bcf972";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
 
 }
 
-let apiKey = "c9b707c9a800bc82a565bfc394bcf972";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=c9b707c9a800bc82a565bfc394bcf972&units=metric";
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
 
 
 let form = document.querySelector("#search-form");
