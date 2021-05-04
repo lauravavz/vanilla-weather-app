a6a359d6932b2699bc9645e013d9e213e38b3110
Laura Vavz <vavzviewz@gmail.com>
2021-05-04 15:58:00 +0200

    Integrated API forecast to App display in JS

--- src/app.js
+++ src/app.js
@@ -13,29 +13,37 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tues", "Wed", "Thu"];
-  days.forEach(function(day) {
+  
+  forecast.forEach(function(forecastDay, index) {
+    if (index < 6) {
     forecastHTML = forecastHTML +
      `     <div class="col-2">
             <div class="weather-forcast-date">
-                ${day}
+                ${formatDay(forecastDay.dt)}
             </div>
-            <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="weather-icon" width="42">
+            <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="weather-icon" width="42">
             <div class="weather-forcast-temperature">
                 <span class="weather-forcast-temperature-max">
-                    18°
+                    ${Math.round(forecastDay.temp.max)}°
                 </span>
                 <span class="weather-forcast-temperature-min">
-                    12°
+                    ${Math.round(forecastDay.temp.min)}°
                 </span>
             </div>
         </div>`;
-
+}
   })
 
   
